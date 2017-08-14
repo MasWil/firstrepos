1407496ead2a567994f2c35ee39520410b718c6e
MasWil <30875257+MasWil@users.noreply.github.com>
2017-08-14 08:55:20 -1000

    added 2 files
    added period1.html and script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function sayHello() {
+  //I create a function called sayHello//
+  var userName = prompt("What is your name?");
+  //I create variable called userName and ask the user something//
+  window.alert("Hello " + userName);
+  //I create a pop up window telling the userName Hello.//
+}
