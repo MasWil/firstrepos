8bd4cd88f5f46d55e2e3d5764b40075c34deaba0
MasWil <30875257+MasWil@users.noreply.github.com>
2017-08-14 09:09:06 -1000

    Updated files
    added 2 files

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ function sayHello() {
   //I create a function called sayHello//
   var userName = prompt("What is your name?");
   //I create variable called userName and ask the user something//
-  window.alert("Hello " + userName);
   //I create a pop up window telling the userName Hello.//
+  var userNickName = prompt("What is your nickname?");
+  var userLastName = prompt("What is your last name?");
+  window.alert("Hello " + userName + " " + userNickName + " " + userLastName);
+  
 }
