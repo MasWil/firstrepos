5b4cdfc86e033600e73a95734f933e496071ab2e
MasWil <30875257+MasWil@users.noreply.github.com>
2017-08-16 09:00:02 -1000

    changed
    yea

--- array.js
+++ array.js
@@ -1,6 +1,7 @@
 var favFoods = ["bacon", "lettuce", "Tomato"];
-//an Array of favFoods//
-var students = ["Michael",  "Raene", "Chuck", "Ryan"];
-//an array of students//
-window.alert(students[2] + " " + favFoods[0]);
-//calling the student in the 2 integer of students and 0 integer of favFoods//
+var newFood = prompt("What do you want to replace bacon with?");
+//ask user for new food and replace with new food
+favFoods[0] = newFood;
+//replace index 0 with what user said
+window.alert(favFoods[0] + " " + favFoods[1] + " " + favFoods[2])
+//output all values of the array to the screen
