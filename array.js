1fea8867dc4608292f52816036ce45d070cd0519
MasWil <30875257+MasWil@users.noreply.github.com>
2017-08-16 08:45:09 -1000

    added array.js
    yea

---
+++ array.js
@@ -0,0 +1,6 @@
+var favFoods = ["bacon", "lettuce", "Tomato"];
+//an Array of favFoods//
+var students = ["Michael",  "Raene", "Chuck", "Ryan"];
+//an array of students//
+window.alert(students[2] + " " + favFoods[0]);
+//calling the student in the 2 integer of students and 0 integer of favFoods//
